05a6d2c45619f35b8dc86a973f81cc8fd72945c2
github99er <ryanmurphycorp@protonmail.com>
2020-04-24 12:33:20 -0700

    Updates

---
+++ precache-manifest.f13f954fe53b728ddbba529e985ab4a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56c14fec0ff387ca31fd174a737629c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "79b730c762a38a9044c2",
+    "url": "/portfolio/static/css/main.45ba9f8f.chunk.css"
+  },
+  {
+    "revision": "5448a0514ba8fd0fca0f",
+    "url": "/portfolio/static/js/2.6c0e796e.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/portfolio/static/js/2.6c0e796e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b730c762a38a9044c2",
+    "url": "/portfolio/static/js/main.a99e1c94.chunk.js"
+  },
+  {
+    "revision": "21b2258191f952c8fe53",
+    "url": "/portfolio/static/js/runtime-main.02c09053.js"
+  },
+  {
+    "revision": "fc2650d003cd24abb51d38c2e609f023",
+    "url": "/portfolio/static/media/RMRESUMEAPRIL.fc2650d0.pdf"
+  }
+]);
